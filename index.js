89ed7e9fdb38832e3ece19bee55bbe514afadf03
John-Henry Liberty <johnhenry.liberty@gmail.com>
2015-09-14 03:41:21 -0500

    updates fonticon import to es6, fixes typo

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-/* jshint node: true */
 'use strict';
 
 module.exports = {
-  name: 'semantic-ui-seeds'
+  name: 'semantic-ui-seeds',
 
   included: function (app) {
-    app.import(app.bowerDirectory + "/semantic-ui/dist/semantic.css")
-    app.import(app.bowerDirectory + "/semantic-ui/dist/semantic.js")
+    app.import(app.bowerDirectory + "/semantic-ui/dist/semantic.css");
+    app.import(app.bowerDirectory + "/semantic-ui/dist/semantic.js");
 
-    var fontExtensions = ['.eot','.otf','.svg','.ttf','.woff','.woff2'];
-    for (var i = fontExtensions.length - 1; i >= 0; i--) {
-      app.import(app.bowerDirectory + "bower_components/semantic-ui/dist/themes/default/assets/fonts/icons"+fontExtensions[i], { destDir: "assets/themes/default/assets/fonts" });
-    };
+    const fontExtensions = ['.eot','.otf','.svg','.ttf','.woff','.woff2'];
+    fontExtensions.forEach((ext) => {
+      app.import(`${app.bowerDirectory}/semantic-ui/dist/themes/default/assets/fonts/icons+${ext}`, { destDir: "assets/themes/default/assets/fonts" })
+    })
   }
 };
