504666d5d742b141897ac0452e427deeec91eed4
John-Henry Liberty <johnhenry.liberty@gmail.com>
2015-09-17 11:25:49 -0500

    change const to var to make sure seeds works on node v0.12+

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
     app.import(app.bowerDirectory + "/semantic-ui/dist/semantic.css");
     app.import(app.bowerDirectory + "/semantic-ui/dist/semantic.js");
 
-    const fontExtensions = ['.eot','.otf','.svg','.ttf','.woff','.woff2'];
+    var fontExtensions = ['.eot','.otf','.svg','.ttf','.woff','.woff2'];
     fontExtensions.forEach((ext) => {
       app.import(`${app.bowerDirectory}/semantic-ui/dist/themes/default/assets/fonts/icons${ext}`, { destDir: "assets/themes/default/assets/fonts" })
     })
